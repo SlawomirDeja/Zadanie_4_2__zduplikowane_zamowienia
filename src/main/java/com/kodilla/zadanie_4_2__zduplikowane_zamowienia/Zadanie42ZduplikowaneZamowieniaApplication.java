40e45057d41db672aa4e51a4ca0054ab3f911436
SlawomirDeja <u0540579977@gmail.com>
2026-03-28 12:55:23 +0100

    Commit1

---
+++ src/main/java/com/kodilla/zadanie_4_2__zduplikowane_zamowienia/Zadanie42ZduplikowaneZamowieniaApplication.java
@@ -0,0 +1,23 @@
+package com.kodilla.zadanie_4_2__zduplikowane_zamowienia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.*;
+
+@SpringBootApplication
+public class Zadanie42ZduplikowaneZamowieniaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Zadanie42ZduplikowaneZamowieniaApplication.class, args);
+
+        Set<Integer> orders = new HashSet<>();
+
+        orders.add(2);
+        orders.add(4);
+        orders.add(6);
+        orders.add(8);
+        orders.add(10);
+    }
+
+}
---
+++ src/test/java/com/kodilla/zadanie_4_2__zduplikowane_zamowienia/Zadanie42ZduplikowaneZamowieniaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.zadanie_4_2__zduplikowane_zamowienia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Zadanie42ZduplikowaneZamowieniaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
